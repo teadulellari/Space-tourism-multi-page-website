65b607e7ba6a9a8957155fad1c93cfca05b9535d
teadulellari <teadulellari@gmail.com>
2024-02-02 16:43:13 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Slogan from "./Slogan"
+import "../styling/global.css"
+
+function App() {
+
+  return (
+    
+   <Slogan />
+   
+  )
+}
+
+export default App;
