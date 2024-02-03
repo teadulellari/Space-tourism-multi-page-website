4e0597f15fe8fd76c065721f043e32d88ec975d5
teadulellari <teadulellari@gmail.com>
2024-02-03 04:09:48 +0200

    main page styling

--- src/components/Slogan.jsx
+++ src/components/Slogan.jsx
@@ -1,19 +1,21 @@
-import Navbar from "./NavBar";
+import NavBar from "./NavBar";
 const Slogan = () => {
     return (
       <div>
-       <Navbar />
-  
+       <NavBar />
+        <div className="container">
         <h2>So, you want to travel to</h2>
-        <h1>Space</h1>
+        <h1>SPACE</h1>
         <p>
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well, sit
           back, and relax because we’ll give you a truly out-of-this-world
           experience!
         </p>
-  
+        </div>
+        <div>
         <button>Explore</button>
+        </div>
       </div>
     );
   };
